26f6d03e5157d8c2614c976ea20e4832de033080
ArturT <mr.arturio@gmail.com>
2023-10-06 18:19:49 -0400

    remove and import game drowings file

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
 import { Ball } from "./modules/ball.js";
 import { DIRECTION } from "./modules/directions.js";
 import { Ai } from "./modules/ai.js";
+import { drawGame } from "./modules/drawGame.js";
 
 const rounds = [5, 5, 3, 3, 2];
 const colors = ["#1abc9c", "#2ecc71", "#3498db", "#8c52ff", "#9b59b6"];
@@ -204,88 +205,10 @@ const Game = {
     }
   },
 
-  // Draw the objects to the canvas element
-  draw: function () {
-    // Clear the Canvas
-    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-
-    // Set the fill style to black
-    this.context.fillStyle = this.color;
-
-    // Draw the background
-    this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
-
-    // Set the fill style to white (For the paddles and the ball)
-    this.context.fillStyle = "#ffffff";
-
-    // Draw the Player
-    this.context.fillRect(
-      this.player.x,
-      this.player.y,
-      this.player.width,
-      this.player.height
-    );
-
-    // Draw the Ai
-    this.context.fillRect(this.ai.x, this.ai.y, this.ai.width, this.ai.height);
-
-    // Draw the Ball
-    if (Pong._turnDelayIsOver.call(this)) {
-      this.context.fillRect(
-        this.ball.x,
-        this.ball.y,
-        this.ball.width,
-        this.ball.height
-      );
-    }
-
-    // Draw the net (Line in the middle)
-    this.context.beginPath();
-    this.context.setLineDash([7, 15]);
-    this.context.moveTo(this.canvas.width / 2, this.canvas.height - 140);
-    this.context.lineTo(this.canvas.width / 2, 140);
-    this.context.lineWidth = 10;
-    this.context.strokeStyle = "#ffffff";
-    this.context.stroke();
-
-    // Set the default canvas font and align it to the center
-    this.context.font = "100px Courier New";
-    this.context.textAlign = "center";
-
-    // Draw the players score (left)
-    this.context.fillText(
-      this.player.score.toString(),
-      this.canvas.width / 2 - 300,
-      200
-    );
-
-    // Draw the paddles score (right)
-    this.context.fillText(
-      this.ai.score.toString(),
-      this.canvas.width / 2 + 300,
-      200
-    );
-
-    // Change the font size for the center score text
-    this.context.font = "30px Courier New";
-
-    // Draw the winning score (center)
-    this.context.fillText(
-      "Round " + (Pong.round + 1),
-      this.canvas.width / 2,
-      35
-    );
-
-    // Change the font size for the center score value
-    this.context.font = "40px Courier";
-
-    // Draw the current round number
-    this.context.fillText(
-      rounds[Pong.round] ? rounds[Pong.round] : rounds[Pong.round - 1],
-      this.canvas.width / 2,
-      100
-    );
-  },
+    // Draw the objects to the canvas element
+    draw: function () {
+        drawGame(this, rounds); // Use the drawGame function from the draw module
+      },
 
   loop: function () {
     Pong.update();
