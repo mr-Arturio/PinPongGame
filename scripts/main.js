b5ac79706c1172426774ba304c833026d51c1c5f
ArturT <mr.arturio@gmail.com>
2023-10-05 23:02:54 -0400

    import some code logic

--- scripts/main.js
+++ scripts/main.js
@@ -1,29 +1,10 @@
-// Global Constants
-const DIRECTION = {
-  IDLE: 0,
-  UP: 1,
-  DOWN: 2,
-  LEFT: 3,
-  RIGHT: 4
-};
+import { Ball } from "./modules/ball.js";
+import { DIRECTION } from "./modules/directions.js";
+
 
 const rounds = [5, 5, 3, 3, 2];
 const colors = ['#1abc9c', '#2ecc71', '#3498db', '#8c52ff', '#9b59b6'];
 
-// The ball object (The cube that bounces back and forth)
-const Ball = {
-  new(incrementedSpeed) {
-      return {
-          width: 18,
-          height: 18,
-          x: (this.canvas.width / 2) - 9,
-          y: (this.canvas.height / 2) - 9,
-          moveX: DIRECTION.IDLE,
-          moveY: DIRECTION.IDLE,
-          speed: incrementedSpeed || 7
-      };
-  }
-};
 
 // The ai object (The two lines that move up and down)
 const Ai = {
